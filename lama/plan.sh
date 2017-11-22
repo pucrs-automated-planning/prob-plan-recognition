14bbbc233c6b241465bc26f1824536755e7b9572
Felipe Meneguzzi <felipe.meneguzzi@gmail.com>
2017-11-22 14:35:41 -0200

    Fixes throughout the code.

--- lama/plan.sh
+++ lama/plan.sh
@@ -2,8 +2,8 @@
 
 # Command line arguments seem lsto be fine, run planner
 echo "1. Running translator"
-python /Users/ramonfragapereira/Workspace/MyRecogniser/lama/translate/translate.py $1 $2
+python ./translate/translate.py $1 $2
 echo "2. Running preprocessor"
-/Users/ramonfragapereira/Workspace/MyRecogniser/lama/preprocess/preprocess < output.sas
+./preprocess/preprocess < output.sas
 echo "3. Running search"
-/Users/ramonfragapereira/Workspace/MyRecogniser/lama/search/search l $3 < output
+./search/search l $3 < output
