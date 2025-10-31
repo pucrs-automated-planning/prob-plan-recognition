15f9c4dbeda17695efb0510bcb5b34cfc9b0d37a
Kin Max Gusmão <gusmaokin@gmail.com>
2025-10-31 01:14:35 -0300

    My cursed rendition of work dirs for this repo

---
+++ make_dirs.sh
@@ -0,0 +1,7 @@
+# create a directory and any necessary parent directories given the only argument passed to this script
+mkdir -p "$1"
+cp -r *.py "$1"
+cp -r plan-greedy "$1"
+cp -r pr "$1"
+cp -r lama "$1"
+cp -r hspf "$1"
--- slurm_single_benchmark.sh
+++ slurm_single_benchmark.sh
@@ -7,6 +7,13 @@
 module load miniconda3
 conda activate prob-plan-recognition
 
+# use the arguments in the work dir name
+WORK_DIR_NAME=$(echo "${@}" | tr ' ' '-')
+
+bash ./make_dirs.sh "work-dir-${WORK_DIR_NAME}"
+
+cd "work-dir-${WORK_DIR_NAME}"
+
 srun python3 run_domain_obs.py "${@:1}" &
 
 wait
