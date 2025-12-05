ceba0656592fa6752e5966695940e2d9960f64b2
Kin Max Gusmão <gusmaokin@gmail.com>
2025-12-05 01:54:29 -0300

    Add work-dir cleanup

--- make_dirs.sh
+++ make_dirs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -l
 
 # create a directory and any necessary parent directories given the only argument passed to this script
+rm -rf "$1" 2>/dev/null || true
 mkdir -p "$1"
 cp -r *.py "$1"
 cp -r plan-greedy "$1"
